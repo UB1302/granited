2f0e854322b92ee8b52c876eb9bac4a8cee2e715
UB1302 <uttkarshbansal13@gmail.com>
2021-09-23 17:53:33 +0530

    Added ability to delete a task

--- app/javascript/src/components/Tasks/Table/TableRow.jsx
+++ app/javascript/src/components/Tasks/Table/TableRow.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 import PropTypes from "prop-types";
 
-const TableRow = ({ data, showTask }) => {
+const TableRow = ({ data, destroyTask, showTask }) => {
   return (
     <tbody className="bg-white divide-y divide-gray-200">
       {data.map(rowData => (
@@ -15,7 +15,7 @@ const TableRow = ({ data, showTask }) => {
           </td>
           <td
             className="px-6 py-4 text-sm font-medium
-            leading-5 text-bb-gray whitespace-no-wrap"
+            leading-5 text-gray-900 whitespace-no-wrap"
           >
             {rowData.user_id}
           </td>
@@ -27,6 +27,18 @@ const TableRow = ({ data, showTask }) => {
               Show
             </a>
           </td>
+          <td
+            className="px-6 py-4 text-sm font-medium
+            leading-5 text-right cursor-pointer"
+          >
+            <a
+              className="text-red-500
+              hover:text-red-700"
+              onClick={() => destroyTask(rowData.slug)}
+            >
+              Delete
+            </a>
+          </td>
         </tr>
       ))}
     </tbody>
@@ -34,7 +46,9 @@ const TableRow = ({ data, showTask }) => {
 };
 
 TableRow.propTypes = {
-  data: PropTypes.array.isRequired
+  data: PropTypes.array.isRequired,
+  destroyTask: PropTypes.func,
+  showTask: PropTypes.func
 };
 
 export default TableRow;
