b3fceb3cef6b5bb6d9eec6c8aa59e3bdabcee2af
UB1302 <uttkarshbansal13@gmail.com>
2021-10-02 18:39:17 +0530

    Added progress to tasks

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,8 +1,10 @@
 # frozen_string_literal: true
 
 class Task < ApplicationRecord
+  RESTRICTED_ATTRIBUTES = %i[title user_id]
   validates :title, presence: true, length: { maximum: 50 }
   belongs_to :user
+  enum progress: { pending: 0, completed: 1 }
   has_many :comments, dependent: :destroy
   validates :slug, uniqueness: true
   validate :slug_not_changed
